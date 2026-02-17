9ce6c2c14f578eacbc83939fa9caf003bf76e068
rustbasic <127506429+rustbasic@users.noreply.github.com>
2026-02-17 12:41:35 +0900

    ezclip v0.5.5
    ezclip v0.5.5

--- sw.js
+++ sw.js
@@ -11,11 +11,27 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
+    }).then(() => {
+      return self.skipWaiting();
     })
   );
 });
 
-/* Serve cached content when offline */
+/* Clean up outdated caches and take control of all clients immediately */
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
